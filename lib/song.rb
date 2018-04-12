2dc56f6caf0fa8d2488e7b12da12609433f59fbd
Sean Febo <seanfebo@techie.com>
2018-04-12 05:00:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,38 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  def self.create_by_name(name)
+    song = self.new
+    @@all << song
+    song.name = name
+    song
+  end
+  def self.find_by_name(name)
+    @@all.detect{|song|song.name==name}
+  end
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+  def self.alphabetical
+    @@all.sort_by{ |i| i.name }
+  end
+  def self.new_from_filename(filename)
+    song = song.new
+    new_from_filename = filename.chomp(".mp3").split()
+    
+    
 end
